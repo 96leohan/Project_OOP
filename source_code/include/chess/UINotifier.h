8bb600c78ee2fc4fc655feeb478e117517ad0485
96leohan <129765801+96leohan@users.noreply.github.com>
2025-05-31 00:17:10 +0700

    Delete source_code/include/chess/UINotifier.h

--- source_code/include/chess/UINotifier.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Position.h"
-
-class UINotifier {
-public:
-    virtual ~UINotifier() = default;
-    virtual void notifyMove(const Position& from, const Position& to) = 0;
-};
